7d341292301abaf246d88e267767f313b2c32bf4
AluBhorta <dev.bhorta@gmail.com>
2019-06-13 12:05:56 +0600

    tour of heroes completed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { HeroesComponent } from "./components/heroes/heroes.component";
+import { DashboardComponent } from "./components/dashboard/dashboard.component";
+import { HeroDetailComponent } from "./components/hero-detail/hero-detail.component";
+import { MessagesComponent } from "./components/messages/messages.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "heroes", component: HeroesComponent },
+  { path: "messages", component: MessagesComponent },
+  { path: "dashboard", component: DashboardComponent },
+  { path: "", redirectTo: "dashboard", pathMatch: "full" },
+  { path: "detail/:id", component: HeroDetailComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
